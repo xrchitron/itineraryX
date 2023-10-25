2b06308c27a786bce9f707da487186c17eb63ae8
xrchitron <yuahochen1124@gmail.com>
2023-10-26 03:24:23 +0800

    test: environment properties

--- server/app.js
+++ server/app.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
@@ -20,8 +18,8 @@ app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public/404.html'))
 })
 
-const server = app.listen(3000, () => {
-  console.log('Server is listening on port 3000')
+const server = app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is listening on port ${process.env.SERVER_PORT}`)
 })
 // try to deploy again
 // Error handling
