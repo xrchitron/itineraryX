f26c89519030c01272b417d070d4f1a7f6f313cc
xrchitron <yuahochen1124@gmail.com>
2023-10-25 15:54:09 +0800

    build: project init

---
+++ client/vite.config.js
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    hmr:{
+      protocol: 'ws',
+      host: 'localhost',
+      port: 3000,
+      path: '/ws',
+      timeout: 20000,
+      overlay: true,
+      clientPort: 3050,
+    }
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
---
+++ server/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+const { SERVER_PORT, API_VERSION } = process.env
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+// api routes
+// app.use(`/api/${API_VERSION}`, require('./routes'))
+app.get('/value', (req, res) => {
+  res.send('Hi')
+})
+// page not found
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public/404.html'))
+})
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is listening on port ${SERVER_PORT}`)
+})
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Server error')
+})
