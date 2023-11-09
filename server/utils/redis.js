ea6979a99445a6f36c0ba111eed025e6c5f8b246
xrchitron <yuahochen1124@gmail.com>
2023-11-09 11:16:48 +0800

    fix: redis client settings

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -1,8 +1,10 @@
 const redis = require('redis')
 const { promisify } = require('util')
 const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  },
   legacyMode: true
 })
 client.connect()
