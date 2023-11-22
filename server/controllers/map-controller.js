c0c53049b2212a62bcc2bc1e8411746c9a9aaba6
Yuhao <yuhaochen1124@gmail.com>
2023-11-22 16:20:04 +0800

    check: element content

--- server/controllers/map-controller.js
+++ server/controllers/map-controller.js
@@ -81,7 +81,7 @@ const mapController = {
       const apiResponse = await mapServices.getDistanceMatrixWithUrl(url)
 
       // absorb elements from response
-      console.log(apiResponse.data)
+      console.log(apiResponse)
       const elements = apiResponse.data.rows[0].elements[0]
 
       // check if routes are found
