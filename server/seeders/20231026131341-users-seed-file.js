d6d340e40b7da640d9ebf86de902bbd22da7c80b
Yuhao <yuhaochen1124@gmail.com>
2023-11-29 19:36:57 +0800

    fix: turn user seeder file to transaction

--- server/seeders/20231026131341-users-seed-file.js
+++ server/seeders/20231026131341-users-seed-file.js
@@ -3,37 +3,29 @@
 const bcrypt = require('bcryptjs')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', [{ // add 5 users
-      email: 'user01@example.com',
-      password: await bcrypt.hash('12345678', 10),
-      name: 'user01',
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
-      email: 'user02@example.com',
-      password: await bcrypt.hash('12345678', 10),
-      name: 'user02',
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
-      email: 'user03@example.com',
-      password: await bcrypt.hash('12345678', 10),
-      name: 'user03',
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
-      email: 'user04@example.com',
-      password: await bcrypt.hash('12345678', 10),
-      name: 'user04',
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
-      email: 'user05@example.com',
-      password: await bcrypt.hash('12345678', 10),
-      name: 'user05',
-      created_at: new Date(),
-      updated_at: new Date()
-    }], {})
+    let transaction
+    try {
+      transaction = await queryInterface.sequelize.transaction()
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash('password', salt)
+      await queryInterface.bulkInsert('Users',
+        Array.from({ length: 5 }).map((_, i) =>
+          ({
+            email: `user0${i + 1}@example.com`,
+            password: hash,
+            name: `user0${i + 1}`,
+            created_at: new Date(),
+            updated_at: new Date()
+          })
+        ),
+        { transaction }
+      )
+
+      await transaction.commit()
+    } catch (err) {
+      if (transaction) await transaction.rollback()
+      throw err
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
