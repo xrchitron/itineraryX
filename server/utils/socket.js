d04839f8b945916802daa1376ead5dd7a545f859
Yuhao <yuhaochen1124@gmail.com>
2023-12-13 11:25:04 +0800

    feat: add client side url in cors

--- server/utils/socket.js
+++ server/utils/socket.js
@@ -5,7 +5,7 @@ const { Server } = require('socket.io')
 async function connect (server) {
   const io = new Server(server, {
     cors: {
-      origin: ['http://localhost:5173', 'http://localhost:3000'],
+      origin: ['https://itinerary-x.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     }
   })
